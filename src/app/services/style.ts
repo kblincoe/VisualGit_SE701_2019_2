76a0fbcb580b646dfbeae43ef1171d2f14767a37
pcas121 <pcas121@aucklanduni.ac.nz>
2019-03-14 13:01:43 +1300

    Add themes to project, and reload set theme on starting application

--- src/app/services/style.ts
+++ src/app/services/style.ts
@@ -7,31 +7,58 @@ import { logger } from 'logger';
 import { Theme, defaultTheme } from 'model/themes';
 import { promises as fs  } from 'fs';
 
+import { promises as fs } from 'fs';
+import { Open_Preferences } from 'src/open_preferences';
+
 @Injectable({providedIn: 'root'})
 export class StyleService {
   public constructor(
     ngZone: NgZone
   ) {
     electron.ipcRenderer.on('set-theme', (e, theme) => ngZone.run(() => this.setTheme(theme)));
-  }
-
-  public getColours() {
-
+	
+	
+	
+    // This will throw if the file doesn't exist, but that doesnt matter too much as it then wont affect our themes.
+    this.openPreferences().then(theme =>
+      this.themeSubject.next(theme)
+    ).catch(error => {
+      logger.debug("No theme file found, defaulting to original theme");
+      this.themeSubject.next(defaultTheme);
+    });
   }
 
   public listThemes(): Theme[] {
     return Object.keys(Theme).map(k => Theme[k]);
   }
+
   public setTheme(theme: Theme) {
     logger.info("Setting theme to " + JSON.stringify(theme));
 
     this.themeSubject.next(theme);
+
+    // Note: This operation is asynchronous and will not be completed before setTheme returns.
+    this.savePreferences(theme);
   }
+
   public getTheme() {
     return this.themeSubject.getValue();
   }
+
   public observeTheme() {
     return this.themeSubject.asObservable();
+  }
+
+  // Saves the given theme to the theme-setting file
+  private async savePreferences(theme: Theme) {
+    const file = {color: theme};
+    await fs.writeFile(".\\.app\\theme_preference.json", JSON.stringify(file),  {encoding: 'utf-8'});
+  }
+
+  // Loads a theme from the theme-setting file
+  private async openPreferences(): Promise<Theme> {
+    const file = JSON.parse(await fs.readFile(".\\.app\\theme_preference.json", "utf8"));
+    return file.color;
   }
 
   public listTemplates(): string[] {
