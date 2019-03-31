5e4837bb03e4c76805ea1ebaba76b8fa521cbbe0
Chen <czha959@aucklanduni.ac.nz>
2019-03-31 13:39:14 +1300

    add logic to show and hide progress bar

--- src/app/views/select/progressbar.component.ts
+++ src/app/views/select/progressbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "node_modules/@angular/core";
+import construct = Reflect.construct;
 
 @Component({
   selector: "app-select-screen-progressbar",
@@ -8,7 +9,24 @@ import { Component, OnInit } from "node_modules/@angular/core";
 
 
 export class ProgressbarComponent implements OnInit{
+
+  value = 0;
+  isHidden: boolean;
+  public constructor() {
+
+  }
   ngOnInit(): void {
+    this.isHidden = true;
+  }
+  public setValue(value) {
+    this.value = value;
+  }
+  public displayPanel() {
+    this.isHidden = false;
+  }
+
+  public hidePanel() {
+    this.isHidden = true;
   }
 
 
